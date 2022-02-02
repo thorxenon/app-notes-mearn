fcfab808805c011ea90477a1a4cccca86377d862
Pedro Lucas <pedrolucasrangel1@hotmail.com>
2022-02-02 14:14:59 -0300

    deploy finished

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import { connectDB } from './config/db';
 import userRoutes from './routes/userRoutes';
 import noteRoutes from './routes/noteRouter'
 import { errorHandler, notFound } from './middlewares/errorMiddleware';
+import path from 'path'
 
 dotenv.config()
 const app = express();
@@ -15,6 +16,24 @@ connectDB();
 app.use(express.json());
 app.use('/api/users',userRoutes);
 app.use('/api/notes', noteRoutes);
+
+// ----------------- deployment ----------------------
+
+__dirname = path.resolve();
+
+if(process.env.NODE_ENV as string === 'production'){
+    app.use(express.static(path.join('../frontend/build')));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+    })
+}else{
+    app.get('/', (req, res) =>{
+        res.send('API is running...');
+    })
+}
+
+// ----------------- deployment ----------------------
 
 app.use(notFound);
 app.use(errorHandler);
